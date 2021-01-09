fc83a05e64006109257c4c04129007ff81546743
Nicola Cimmino <nicola.cimmino@gmail.com>
2021-01-09 09:24:01 +0100

    Show time and temperature

--- TemperatureLogger.ino
+++ TemperatureLogger.ino
@@ -1,5 +1,5 @@
 //
-//  A palm shaped lamp showing the current time in COlour coded Roman Numerals.
+//  Temperature logger.
 //
 //  Copyright (C) 2019 Nicola Cimmino
 //
@@ -19,14 +19,57 @@
 
 #include <DCServices.h>
 #include <uRTCLib.h>
+#include "uEEPROMLib.h"
 #include <SSD1306AsciiAvrI2c.h>
 #include <Sodaq_SHT2x.h>
 
 uRTCLib *rtc;
 DCServices *dcServices;
 SSD1306AsciiAvrI2c *oled;
+uEEPROMLib *eeprom;
 
 #define DISPLAY_I2C_ADDRESS 0x3C
+
+void displayTime()
+{
+    char text[16];
+    char textB[6];
+
+    rtc->refresh();
+
+    oled->set2X();
+    sprintf(text, "%02i:%02i:%02i", rtc->hour(), rtc->minute(), rtc->second());
+    oled->setCursor(20, 2);
+    oled->print(text);
+
+    float temperature = (SHT2x.GetTemperature() * 10) / 10.0;
+    float humidity = round(SHT2x.GetHumidity());
+
+    sprintf(text, "%sC", dtostrf(temperature, 3, 1, textB));
+    oled->setCursor(0, 6);
+    oled->print(text);
+
+    sprintf(text, "%s%%", dtostrf(humidity, 3, 0, textB));
+    oled->setCursor(80, 6);
+    oled->print(text);
+}
+
+void syncTime()
+{
+    for (uint8_t ix = 0; ix < 6; ix++)
+    {
+        bool syncOK = dcServices->syncRTCToTimeBroadcast();
+        oled->setCursor(0, 1);
+        oled->print(ix % 2 == 0 ? "Timesync...." : "                ");
+
+        if (syncOK)
+        {
+            oled->setCursor(0, 1);
+            oled->print("TS                 ");
+            return;
+        }
+    }
+}
 
 void setup()
 {
@@ -34,6 +77,9 @@ void setup()
     Serial.begin(9600);
 
     rtc = new uRTCLib(0x68);
+
+    eeprom = new uEEPROMLib(0x57);
+
     dcServices = new DCServices(DC_RADIO_NRF24_V2, rtc);
 
     oled = new SSD1306AsciiAvrI2c();
@@ -41,36 +87,15 @@ void setup()
     oled->setFont(System5x7);
     oled->set1X();
     oled->clear();
+
+    syncTime();
 }
 
 void loop()
 {
-    char text[16];
-
-    rtc->refresh();
-
-    if (rtc->second() % 10 == 0)
-    {
-        bool syncOK = dcServices->syncRTCToTimeBroadcast();
-        oled->setCursor(75, 1);
-        oled->print(syncOK ? "*" : " ");
-    }
-
-    sprintf(text, "%02i:%02i:%02i", rtc->hour(), rtc->minute(), rtc->second());
-    oled->setCursor(80, 1);
-    oled->print(text);
-
-    float temperature = SHT2x.GetTemperature() - 1;
-    oled->setCursor(0, 2);
-    oled->print(temperature);
-    oled->print(" C");
-
-    float humidity = SHT2x.GetHumidity();
-    oled->setCursor(0, 3);
-    oled->print(humidity);
-    oled->print(" %");
+    displayTime();
 
     dcServices->loop();
 
-    delay(1000);
+    delay(500);
 }
