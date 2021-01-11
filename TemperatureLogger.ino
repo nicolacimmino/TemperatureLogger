0098c17be49735bcf4b3d781c2643cd6adc0b30f
Nicola Cimmino <nicola.cimmino@gmail.com>
2021-01-11 19:38:43 +0100

    Add battery meter, power save simple mode, change to log every 5 min

--- TemperatureLogger.ino
+++ TemperatureLogger.ino
@@ -17,6 +17,8 @@
 //    along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
+#include <Arduino.h>
+#include <avr/sleep.h>
 #include <Wire.h>
 #include <uRTCLib.h>
 #include "uEEPROMLib.h"
@@ -31,7 +33,7 @@
 #define PLOT_Y_TOP 16
 #define PLOT_Y_BOTTOM (SCREEN_HEIGHT - 5)
 #define PLOT_Y_PIXELS (PLOT_Y_BOTTOM - PLOT_Y_TOP)
-#define PLOT_X_LEFT 4
+#define PLOT_X_LEFT 12
 #define PLOT_X_RIGHT (SCREEN_WIDTH - 4)
 #define PLOT_X_PIXELS (PLOT_X_RIGHT - PLOT_X_LEFT)
 #define RAMLOG_LENGTH_BYTES PLOT_X_PIXELS
@@ -40,16 +42,53 @@
 #define TEMPERATURE_NOT_SET 0xFF
 #define PIN_BUTTON_A 6
 #define DISPLAY_MODES 2
-uint8_t mode = 0;
+#define POWER_SAVE_TIMEOUT_MS 20000
+#define RECORD_DATA_INTERVAL_MS 300000 // 5 minutes
 
 uRTCLib *rtc;
 Adafruit_SSD1306 *oled;
 uEEPROMLib *eeprom;
-bool timeSyncOK = false;
+uint8_t mode = 0;
 uint16_t logStartPointer = 0;
 uint16_t logEndPointer = 0;
 uint8_t ramLog[RAMLOG_LENGTH_BYTES];
 uint8_t ramLogPointer = 0;
+unsigned long lastButtonActivityTime = millis();
+bool powerSaveOn = false;
+
+uint8_t getBatteryLevel()
+{
+    // See this article for an in-depth explanation.
+    // https://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
+    // tl;dr: we switch the ADC to measure the internal 1.1v reference using Vcc as reference, the rest is simple math.
+
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+
+    delay(2);
+    ADCSRA |= _BV(ADSC);
+    while (bit_is_set(ADCSRA, ADSC))
+        ;
+
+    long measuredVcc = 1125300L / (ADCL | (ADCH << 8));
+    analogReference(DEFAULT);
+
+    return min(max((measuredVcc - 2700) / 7, 0), 100);
+}
+
+void displayBatterLevel()
+{
+    oled->setCursor(100, 5);
+    oled->print(getBatteryLevel());
+    oled->print("%");
+}
+
+void clearDisplay()
+{
+    oled->clearDisplay();
+    oled->setTextSize(1);
+    oled->setTextColor(SSD1306_WHITE);
+    displayBatterLevel();
+}
 
 void displayTime()
 {
@@ -58,20 +97,18 @@ void displayTime()
 
     rtc->refresh();
 
-    oled->clearDisplay();
-    oled->setTextSize(2);
-    oled->setTextColor(SSD1306_WHITE);
-    oled->cp437(true);
+    clearDisplay();
 
     sprintf(text, "%02i:%02i:%02i", rtc->hour(), rtc->minute(), rtc->second());
-    oled->setCursor(18, 16);
+    oled->setTextSize(2);
+    oled->setCursor(18, 21);
     oled->print(text);
 
     float temperature = (SHT2x.GetTemperature() * 10) / 10.0;
     float humidity = round(SHT2x.GetHumidity());
 
-    oled->setTextSize(1);
     sprintf(text, "%sC", dtostrf(temperature, 3, 1, textB));
+    oled->setTextSize(1);
     oled->setCursor(0, 50);
     oled->print(text);
 
@@ -79,25 +116,20 @@ void displayTime()
     oled->setCursor(103, 50);
     oled->print(text);
 
-    if (timeSyncOK)
-    {
-        oled->setCursor(0, 0);
-        oled->print("TS");
-    }
-
     oled->display();
 }
 
 void recordData()
 {
     static unsigned long lastRecord = millis();
-    if (millis() - lastRecord < 2000)
+    if (millis() - lastRecord < RECORD_DATA_INTERVAL_MS)
     {
         return;
     }
     lastRecord = millis();
 
     uint8_t temperatureEncoded = 127 + (SHT2x.GetTemperature() * 2);
+
     // eeprom->eeprom_write(logEndPointer, temperatureEncoded);
     // logEndPointer = (logEndPointer + 1) % LOG_LENGTH_BYTES;
 
@@ -107,6 +139,7 @@ void recordData()
 
 uint8_t temperatrureToYOffset(float temperature)
 {
+    temperature = max(min(PLOT_Y_MAX_C, temperature), PLOT_Y_MIN_C);
     return PLOT_Y_TOP + PLOT_Y_PIXELS - floor((PLOT_Y_PIXELS * (temperature - PLOT_Y_MIN_C)) / (PLOT_Y_MAX_C - PLOT_Y_MIN_C));
 }
 
@@ -119,11 +152,7 @@ uint8_t plotIndexToXOffset(uint8_t ix)
 
 void plotTemperature()
 {
-    oled->clearDisplay();
-    oled->setTextSize(1);
-    oled->setTextColor(SSD1306_WHITE);
-    oled->setCursor(0, 7);
-    oled->print("T x=24h y=15-30C");
+    clearDisplay();
 
     oled->drawLine(PLOT_X_LEFT, PLOT_Y_BOTTOM, PLOT_X_RIGHT, PLOT_Y_BOTTOM, SSD1306_WHITE);
     oled->drawLine(PLOT_X_LEFT, PLOT_Y_BOTTOM, PLOT_X_LEFT, PLOT_Y_TOP, SSD1306_WHITE);
@@ -133,6 +162,14 @@ void plotTemperature()
         oled->drawPixel(ix, temperatrureToYOffset(20), SSD1306_WHITE);
         oled->drawPixel(ix, temperatrureToYOffset(25), SSD1306_WHITE);
         oled->drawPixel(ix, temperatrureToYOffset(30), SSD1306_WHITE);
+        oled->setCursor(0, temperatrureToYOffset(15) - 3);
+        oled->print(15);
+        oled->setCursor(0, temperatrureToYOffset(20) - 3);
+        oled->print(20);
+        oled->setCursor(0, temperatrureToYOffset(25) - 3);
+        oled->print(25);
+        oled->setCursor(0, temperatrureToYOffset(30));
+        oled->print(30);
     }
 
     for (int ix = PLOT_X_LEFT; ix < PLOT_X_RIGHT; ix += PLOT_X_PIXELS / 12)
@@ -166,10 +203,6 @@ void setup()
 
     //eeprom = new uEEPROMLib(0x57);
 
-    //    dcServices = new DCServices(DC_RADIO_NRF24_V2, rtc);
-
-    //timeSyncOK = dcServices->syncRTCToTimeBroadcast();
-
     // Initialize to TEMPERATURE_NOT_SET which is not plotted.
     // This later will be a copy from the last RAMLOG_LENGTH_BYTES in EEPROM.
     memset(ramLog, TEMPERATURE_NOT_SET, RAMLOG_LENGTH_BYTES);
@@ -182,31 +215,67 @@ void setup()
     oled->display();
 }
 
-void loop()
+void managePowerSave()
 {
-    bool modechange = false;
+    if (powerSaveOn)
+    {
+        return;
+    }
 
+    if (millis() - lastButtonActivityTime > POWER_SAVE_TIMEOUT_MS)
+    {
+        powerSaveOn = true;
+        oled->ssd1306_command(SSD1306_DISPLAYOFF);
+    }
+}
+
+void exitPowerSave()
+{
+    powerSaveOn = false;
+    oled->ssd1306_command(SSD1306_DISPLAYON);
+}
+
+void checkButtonA()
+{
     if (digitalRead(PIN_BUTTON_A) == LOW)
     {
+        lastButtonActivityTime = millis();
+
+        while (digitalRead(PIN_BUTTON_A) == LOW)
+        {
+            delay(1);
+        }
+
+        if (powerSaveOn)
+        {
+            exitPowerSave();
+            return;
+        }
+
         mode = (mode + 1) % DISPLAY_MODES;
-         modechange = true;
     }
+}
 
-    if (mode == 0)
-    {
-        displayTime();
-    }
-    else
-    {
-        plotTemperature();
-    }
-
+void loop()
+{
     recordData();
+    managePowerSave();
+    checkButtonA();
 
-    while (modechange && digitalRead(PIN_BUTTON_A) == LOW)
+    if (powerSaveOn)
     {
-        delay(1);
+        return;
     }
 
-    // dcServices->loop();
+    switch (mode)
+    {
+    case 0:
+        displayTime();
+        break;
+    case 1:
+        plotTemperature();
+        break;
+    }
+
+    delay(500);
 }
